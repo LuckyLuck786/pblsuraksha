c4e8a214d498a0f4c25caf829b7ab3865ac53883
SanJana-S06 <sanju.s.06828@gmail.com>
2026-05-15 00:13:54 +0530

    something stupid

--- backend/suraksha_project/settings.py
+++ backend/suraksha_project/settings.py
@@ -150,6 +150,17 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # ─── File Upload Settings ──────────────────────────────────────────────────
 FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50 MB
